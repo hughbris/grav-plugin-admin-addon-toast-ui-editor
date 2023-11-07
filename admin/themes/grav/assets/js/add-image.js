c9d535a94398da214750bda1d241161e2761c8a5
Hugh Barnes <dev@hubns.com>
2023-11-08 01:40:10 +1300

    Add upload and markdown into refactor of add image button

--- admin/themes/grav/assets/js/add-image.js
+++ admin/themes/grav/assets/js/add-image.js
@@ -3,6 +3,20 @@
 	var GravPlugin = GravPlugin || {};
 	GravPlugin['adminAddonToastUiEditor'] = Object.assign((GravPlugin.adminAddonToastUiEditor || {}), {
 
+		uploadMedia: (blob, callback) => {
+			const formData = new FormData();
+			formData.append('file', blob);
+			const imgPath = '/' + window.GravAdmin.config.route.concat('/', formData.get('file').name); // WE DON'T REALLY KNOW IF THAT's CORRECT!!
+
+			const request = new XMLHttpRequest();
+			const reqURI = window.Grav.default.Pages.Page.Media.PageMediaInstances.urls.add.concat(`/admin-nonce:${window.GravAdmin.config.admin_nonce}`);
+			request.open('POST', reqURI, true);
+			request.onload = () => {
+				callback();
+				};
+			request.send(formData);
+			},
+
 		newEditor: (toastUiId, editorInitialValue, fieldCustomOptions) => {
 
 			const editorDefaultOptions = GravPlugin.adminAddonToastUiEditor.configs.defaults;
@@ -27,6 +41,22 @@
 				// TODO: support overrides for instance properties
 				editorDefaultToolbar.remove.forEach( function(itemName) {
 					thisEditor.removeToolbarItem(itemName);
+					});
+
+				thisEditor.addCommand('markdown', 'addMarkdownImage', (settings, ed) => {
+					let url = encodeURI(settings.url);
+					if(Object.hasOwn(settings, 'classes')) {
+						url += '?classes=' + settings.classes.join();
+					}
+
+					let newMarkdown = `![${settings.alt}](${url})`;
+
+					const captionProvided = ( Object.hasOwn(settings, 'caption') && settings.caption.trim().length > 0);
+					if(captionProvided) {
+						newMarkdown = `[figure caption="${settings.caption}"]${newMarkdown}[/figure]`;
+					}
+
+					thisEditor.replaceSelection(newMarkdown);
 					});
 
 				if(typeof boundTextarea !== undefined) {
@@ -78,6 +108,52 @@
 			thisEditor.focus();
 			};
 		fields.cancelButton.addEventListener('click', closePopup);
+
+		fields.newImage.addEventListener('change', (e) => {
+			GravPlugin.adminAddonToastUiEditor.uploadMedia(e.target.files[0], () => {
+				const fileName = fields.newImage.files[0].name;
+				const uploadOptionIndex = fields.url.selectedOptions.item(0).index;
+				fields.url.add(new Option(fileName, fileName, false, true), uploadOptionIndex+1);
+				});
+			});
+
+		fields.url.addEventListener('change', (e) => {
+			if (e.target.selectedOptions.item(0).id == `imgUploader-${thisEditor.options.el.id}`) {
+				fields.newImage.showPicker();
+				/* TODO: refresh the Page Media control here */
+			}
+			else {
+				if(Object.hasOwn(mediaListOnLoad, e.target.value)) {
+					const selectedMediaAsset = mediaListOnLoad[e.target.value];
+					if(fields.caption.value.trim().length == 0 && Object.hasOwn(selectedMediaAsset, 'caption')) {
+						fields.caption.value = selectedMediaAsset.caption;
+					}
+					if(fields.alt.value.trim().length == 0 && Object.hasOwn(selectedMediaAsset, 'alt')) {
+						fields.alt.value = selectedMediaAsset.alt;
+					}
+				}
+				else {
+					/* TODO: handle this */
+				}
+			}
+			});
+
+		form.addEventListener('submit', (e) => {
+			e.preventDefault();
+
+			const values = {
+				url: fields.url.value,
+				alt: fields.alt.value,
+				caption: fields.caption.value,
+				};
+
+			const classes = Array.from(fields.classes.elements).filter(c => c.checked);
+			if (classes.length > 0) {
+				values['classes'] = classes.map(c => c.value);
+			}
+			thisEditor.eventEmitter.emit('command', 'addMarkdownImage', values);
+			closePopup();
+			});
 
 		thisEditor.insertToolbarItem({}, {
 			name: 'markdownImagePopup',
